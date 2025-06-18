70e0b0ca5ec6a37f14ce8338f2786e360e730f72
Yuri Viana <ycviana@sga.pucminas.br>
2025-06-18 18:29:51 -0300

    feat: finalizar projeto

---
+++ script.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+const trash = document.querySelectorAll('.trash')
+
+function createItemList() {
+    const li = document.createElement('li')
+
+    const checkboxWrapper = document.createElement('div')
+
+    const checkImg = document.createElement('img')
+    checkImg.classList.add('checkbox-img')
+    
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.name = 'check'
+    checkbox.id = 'check'
+
+    const nameItem = document.createElement('label')
+    nameItem.setAttribute('for', 'check')
+    nameItem.textContent = input.value
+
+    const trashImg = document.createElement('img')
+    trashImg.src = 'assets/icon-trash.svg'
+    trashImg.classList.add('trash')
+
+    checkboxWrapper.append(checkImg)
+    checkboxWrapper.append(checkbox)
+    checkboxWrapper.append(nameItem)
+
+    li.append(checkboxWrapper)
+    li.append(trashImg)
+
+    return li
+}
+
+ul.addEventListener('click', (event) => {
+    if(event.target.classList.contains('trash')) {
+        const li = event.target.closest('li')
+        if(li) {
+            li.remove()
+        }
+    }
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    ul.append(createItemList())
+
+    input.value = ''
+})
